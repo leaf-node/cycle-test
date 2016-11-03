186bd4640862e92d48a832c25a40c7b32f78e105
Andrew Engelbrecht <sudoman@ninthfloor.org>
2016-11-02 21:09:12 -0400

    migrated to npm assert package
    this ensures the same behavior in nodejs and the browser: halt on failed
    asserts.

--- test/test.js
+++ test/test.js
@@ -10,74 +10,76 @@
 // This program is a part of cycle-test
 
 
-var ct, result;
+var ct, result, assert;
+
+assert = require('assert');
 
 // normally: require('cycle-test');
 ct = require('../lib/main.js');
 
 
 result = ct.hasCycle([[1], [2], [0]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1, 2], [2], []]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[0]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1], [3], undefined, [0]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1, 2], [2], [0]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1, 2], [2], [2]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[]]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[0], []]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1], [2, 4], [0]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[5, 3, 1, 10], [], [], [], [0, 7], [], [2, 8, 9], [], [11, 9], [], [6, 4], [5]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[5, 3, 1], [], [], [], [0, 7], [], [2, 8, 9], [], [11, 9], [], [6, 4], [5]]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[1], [], [3], []]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[1], undefined, [3], undefined]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[1], [0], [3], []]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1], [], [3], [2]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1], undefined, [3], [2]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1, 1, 1], [], [3, 3], []]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[1, 1, 1], [5], [3, 3], []]);
-console.assert(result === false);
+assert(result === false);
 
 result = ct.hasCycle([[1, 1, 1], [], [3, 3], [2]]);
-console.assert(result === true);
+assert(result === true);
 
 result = ct.hasCycle([[1, 1, 1], [5], [3, 3], [2]]);
-console.assert(result === true);
+assert(result === true);
 
 
 console.log("all tests passed.");
